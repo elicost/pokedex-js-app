d6d99e75f9bc985fe1e2fd565bfc8407ffe72d94
elicost <140678342+elicost@users.noreply.github.com>
2023-09-05 14:35:26 -0500

    Create Pokemon array

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let pokemonList = [];
+let pokemonList[1] = {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['water']
+};
+let pokemonList[2] = {
+    name: 'Vulpix',
+    height: 0.6
+    types: ['fire']
+};
+let pokemonList[3] = {
+    name: 'Houndour'
+    height: 0.6
+    types: ['dark', 'fire']
+}
