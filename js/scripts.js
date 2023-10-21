2308e5b6911438870d16597c448a0da9fa367898
elicost <140678342+elicost@users.noreply.github.com>
2023-10-21 15:28:35 -0500

    Corrected array indices

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [];
-pokemonList[1] = {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['water']
-};
-pokemonList[2] = {
-    name: 'Vulpix',
-    height: 0.6,
-    types: ['fire']
-};
-pokemonList[3] = {
-    name: 'Houndour',
-    height: 0.6,
-    types: ['dark', 'fire']
-}
+    pokemonList[0] = {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['water']
+    };
+    pokemonList[1] = {
+        name: 'Vulpix',
+        height: 0.6,
+        types: ['fire']
+    };
+    pokemonList[2] = {
+        name: 'Houndour',
+        height: 0.6,
+        types: ['dark', 'fire']
+    }
