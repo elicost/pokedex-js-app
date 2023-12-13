ab8ff111ba92e00fef365eef553b2a625566fbb9
elicost <140678342+elicost@users.noreply.github.com>
2023-12-13 14:23:51 -0600

    Enclose array in IIFE with requisite keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
-let pokemonList = [];
-    pokemonList[0] = {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
-    };
-    pokemonList[1] = {
-        name: 'Vulpix',
-        height: 0.6,
-        types: ['fire']
-    };
-    pokemonList[2] = {
-        name: 'Houndour',
-        height: 0.6,
-        types: ['dark', 'fire']
+let pokemonRepository = (function () {
+    let pokemonList = [];
+        pokemonList[0] = {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        };
+        pokemonList[1] = {
+            name: 'Vulpix',
+            height: 0.6,
+            types: ['fire']
+        };
+        pokemonList[2] = {
+            name: 'Houndour',
+            height: 0.6,
+            types: ['dark', 'fire']
+        }
+
+    function getAll () {
+        return pokemonList;
     }
 
-function pokemonLoop(pokemon) {
-    if (pokemon.height < 0.6) {
-        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s small!</p>')
-    } else {
-        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')</p>')
+    function add (pokemon) {
+        pokemonList.push(pokemon);
     }
-}
-pokemonList.forEach(pokemonLoop);
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})()
