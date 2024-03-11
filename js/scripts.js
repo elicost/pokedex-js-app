38b1a24fdcdc59704570441b70dbea850deada57
elicost <140678342+elicost@users.noreply.github.com>
2024-03-11 14:44:14 -0500

    Add polyfills and API url

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
-        pokemonList[0] = {
-            name: 'Squirtle',
-            height: 0.5,
-            types: ['water']
-        };
-        pokemonList[1] = {
-            name: 'Vulpix',
-            height: 0.6,
-            types: ['fire']
-        };
-        pokemonList[2] = {
-            name: 'Houndour',
-            height: 0.6,
-            types: ['dark', 'fire']
-        }
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+        // pokemonList[0] = {
+        //     name: 'Squirtle',
+        //     height: 0.5,
+        //     types: ['water']
+        // };
+        // pokemonList[1] = {
+        //     name: 'Vulpix',
+        //     height: 0.6,
+        //     types: ['fire']
+        // };
+        // pokemonList[2] = {
+        //     name: 'Houndour',
+        //     height: 0.6,
+        //     types: ['dark', 'fire']
+        // }
 
     function getAll () {
         return pokemonList;
